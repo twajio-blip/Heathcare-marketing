0f9c7f1461fbc0a57851518eed2ffb258f0c494f
twajio-blip <contact@legacyhealthgrowth.com>
2026-04-07 12:52:44 +0600

    Remove base from vite target for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-    base: '/Heathcare-marketing/',
     plugins: [
         tailwindcss(),
     ],
